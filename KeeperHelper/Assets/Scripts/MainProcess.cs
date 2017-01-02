4e6fdd4aea0c61d8a24f01bbdb9db711fd44823e
Mahoimi <makahomi@gmail.com>
2017-01-02 21:41:30 +0100

    Create Unity project & Hello world

---
+++ KeeperHelper/Assets/Scripts/MainProcess.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+public class MainProcess : SingletonMonoBehaviour<MainProcess>
+{
+    #region Monobehaviour
+    public override void Awake() {
+        Debug.Log("Hello world !");
+	}
+	
+	void Update () {
+		
+	}
+    #endregion
+}
---
+++ KeeperHelper/Assets/Scripts/Utils/SingletonMonoBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils
+{
+    public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        #region Variables
+        private static T m_instance;
+
+        public static T Instance { get { return m_instance; } }
+        #endregion
+
+        #region Monobehaviour
+        protected SingletonMonoBehaviour() { }
+
+        public virtual void Awake()
+        {
+            if (m_instance == null)
+                m_instance = GetComponent<T>();
+        }
+        #endregion
+    }
+}
